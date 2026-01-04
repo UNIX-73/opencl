4fec9e31e42523134f1500095712d5883770c669
Unai Iborra <iborra.unai@gmail.com>
2026-01-04 22:37:04 +0100

    a

--- e9/vadd_c.c
+++ e9/vadd_c.c
@@ -39,7 +39,7 @@ extern int    output_device_info(cl_device_id);
 #define TOL (0.001)     // tolerance used in floating point comparisons
 #define LENGTH (102400) // length of vectors a, b, and c
 
-#define CPU_PERCENTAGE 0.01
+#define CPU_PERCENTAGE 0.001
 
 #define GPU_LEN (size_t)((double)LENGTH * (double)CPU_PERCENTAGE)
 #define CPU_LEN (LENGTH - GPU_LEN)
